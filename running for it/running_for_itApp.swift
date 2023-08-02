25298a5bd26458f2417084ca94fe50ac3d0976a7
Sarah Toll <stoll@uoguelph.ca>
2023-08-01 19:45:40 -0700

    Initial Commit

---
+++ running for it/running_for_itApp.swift
@@ -0,0 +1,17 @@
+//
+//  running_for_itApp.swift
+//  running for it
+//
+//  Created by Sarah Toll on 2023-08-01.
+//
+
+import SwiftUI
+
+@main
+struct running_for_itApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ running for itUITests/running_for_itUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  running_for_itUITestsLaunchTests.swift
+//  running for itUITests
+//
+//  Created by Sarah Toll on 2023-08-01.
+//
+
+import XCTest
+
+final class running_for_itUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
